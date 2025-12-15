29c3a9b0866f7cee3a3b984d4b903f972c989406
João Gabriel <joaogabrdev@gmail.com>
2025-12-15 15:28:36 -0300

    adicionando classe rectangle e exercicio utilizando a mesma

---
+++ entities/Rectangle.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Rectangle {
+    public double width;
+    public double height;
+
+    public double area() {
+        double area = width * height;
+        return area;
+    }
+
+    public double perimeter() {
+        double perimeter = 2 * (width + height);
+        return perimeter;
+    }
+
+    public double diagonal() {
+        double diagonal = Math.sqrt((width * width + height * height));
+        return diagonal;
+    }
+}
---
+++ exercicios/ExercicioRetanguloClass.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import entities.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioRetanguloClass {
+    public static void main(String args[]) {
+        // Fazer um programa para ler os valores da largura e altura de um retângulo. Em seguida, mostrar na tela o valor
+        // de sua área, perímetro e diagonal. Usar uma classe.
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+        System.out.println("Enter rectangle width and height: ");
+        rectangle.width = scanner.nextDouble();
+        rectangle.height = scanner.nextDouble();
+
+        System.out.println();
+        System.out.printf("AREA = %.2f", rectangle.area());
+        System.out.println();
+        System.out.printf("PERIMETER = %.2f", rectangle.perimeter());
+        System.out.println();
+        System.out.printf("DIAGONAL = %.2f", rectangle.diagonal());
+        scanner.close();
+    }
+}
