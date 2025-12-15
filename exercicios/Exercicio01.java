cea6d832e8b7e3f03a3607d788ec80fd1124ec74
João Gabriel <joaogabrdev@gmail.com>
2025-12-15 02:26:29 -0300

    adição da classe Product e o primeiro exercicio com ela

--- exercicios/Exercicio01.java
+++ exercicios/Exercicio01.java
@@ -1,5 +1,7 @@
 package exercicios;
 
+import entities.Triangle;
+
 import java.util.Locale;
 import java.util.Scanner;
 import static java.util.Locale.setDefault;
@@ -7,31 +9,26 @@ import static java.util.Locale.setDefault;
 
 public class Exercicio01 {
     public static void main(String args []) {
-        // Faça um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas válidas).
-        // Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos possui a maior
-        // área.
-        // A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c, é a seguinte:
-        // Area = raiz de p(p-a) * (p - b) * (p - c)
-        // Onde  p = a + b + b / 2
+
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        double xA, xB, xC, yA, yB, yC;
+
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
 
         System.out.println("\nDigite as medidas do triângulo X: ");
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
 
         System.out.println("\nDigite as medidas do triângulo Y: ");
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        double p = (xA + xB + xC) / 2.0;
-        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
-
-        p = (yA + yB + yC) / 2.0;
-        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+        double areaX = x.area();
+        double areaY = y.area();
 
         System.out.printf("Área do triângulo X: %.4f%n", areaX);
         System.out.printf("Área do triângulo y: %.4f%n", areaY);
