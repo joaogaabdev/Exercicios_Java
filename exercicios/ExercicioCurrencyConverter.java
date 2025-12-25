888f425761d16d4fe1bbe4522feccdb32e3dfb72
João Gabriel <joaogabrdev@gmail.com>
2025-12-25 19:52:29 -0300

    subindo exercicio da classe CurrencyConverter

---
+++ exercicios/ExercicioCurrencyConverter.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioCurrencyConverter {
+    public static void main(String[] args) {
+        Locale = Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is dollar price");
+        System.out.println("How many dollars will be bought?");
+        System.out.println("Amount to be paid in reais = ");
+    }
+}
