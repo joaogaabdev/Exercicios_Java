5ebdd2ee85ba83cb1aeddd0d039cbbe2837a4ef9
João Gabriel <joaogabrdev@gmail.com>
2025-12-28 19:35:39 -0300

    adicionando construtor a classe product

--- exercicios/ExercicioProductClass.java
+++ exercicios/ExercicioProductClass.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ExercicioProductClass {
-    public static void main(String args []) {
+    public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
