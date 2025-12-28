1f9157cf76ca644efcecde6ae1fa778e0b1385d6
João Gabriel <joaogabrdev@gmail.com>
2025-12-28 19:58:19 -0300

    adicionando sobrecarga no construtor da classe product

--- entities/Product.java
+++ entities/Product.java
@@ -10,7 +10,11 @@ public class Product {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
 
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
     }
 
     public double totalValueInStock() {
